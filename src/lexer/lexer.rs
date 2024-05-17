7946ba192eb8a48f903d8d42beb2c27df5896a69
Bolvic <victor.bolheme@gmail.com>
2024-05-17 22:04:49 +0200

    ~ | Still trying to fix Lexer::get_token

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -10,28 +10,32 @@ pub enum Token {
 
 pub struct Lexer <'a> {
     file: &'a [u8],
+    file_len: usize,
     current_index: usize,
     tokens: Vec<Token>,
 }
 
 impl <'a> Lexer <'a> {
     pub fn new(file: &'a [u8]) -> Self {
+        let file_len = file.len();
         Lexer {
             file,
+            file_len,
             current_index: 0,
             tokens: vec![],
         }
     }
     
-    pub fn get_token(&'a mut self) -> Token {
+    pub fn get_token(& mut self) -> Token {
         while self.file[self.current_index].is_ascii_whitespace() {
             self.current_index += 1;
         }
         if self.file[self.current_index].is_ascii_alphabetic() {
             let mut identifier = Vec::new();
             identifier.push(self.file[self.current_index]);
-            while self.file[self.current_index].is_ascii_alphanumeric() {
+            while self.current_index < self.current_index && self.file[self.current_index].is_ascii_alphanumeric() {
                 identifier.push(self.file[self.current_index]);
+                self.current_index += 1;
             }
             let identifier = String::from_utf8(identifier).unwrap();
             if identifier == "def" {
@@ -54,16 +58,17 @@ impl <'a> Lexer <'a> {
         }
         if self.file[self.current_index] == b'#' {
             self.current_index += 1;
-            while self.file[self.current_index] != b'\0'
+            while self.current_index < self.file_len
+                && self.file[self.current_index] != b'\0'
                 && self.file[self.current_index] != b'\n'
                 && self.file[self.current_index] != b'\r' {
                 self.current_index += 1;
             }
-            if self.file[self.current_index] != b'\0' {
+            if self.current_index < self.file_len || self.file[self.current_index] != b'\0' {
                 return self.get_token();
             }
         }
-        if self.file[self.current_index] == b'\0' {
+        if self.current_index >= self.file_len || self.file[self.current_index] == b'\0' {
             return Token::TokenEOF;
         }
         let this_char: char = char::from(self.file[self.current_index]);
@@ -82,11 +87,10 @@ mod tests {
         let mut lexer = Lexer::new(test);
 
         assert_eq!(lexer.get_token(), Token::TokenIdentifier("bit".to_string()));
-/*        assert_eq!(lexer.get_token(), Token::TokenIdentifier("bit64".to_string()));
+        assert_eq!(lexer.get_token(), Token::TokenIdentifier("bit64".to_string()));
         assert_eq!(lexer.get_token(), Token::TokenNumber(64.));
         assert_eq!(lexer.get_token(), Token::TokenDef);
         assert_eq!(lexer.get_token(), Token::TokenExtern);
         assert_eq!(lexer.get_token(), Token::TokenEOF);
-*/
     }
 }
